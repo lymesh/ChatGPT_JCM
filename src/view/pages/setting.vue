56bffaf8f4efeb77ec321b1fbc07b31402f8f664
lymesh <30970685+lymesh@users.noreply.github.com>
2023-04-22 15:30:08 +0800

    Update setting.vue

--- src/view/pages/setting.vue
+++ src/view/pages/setting.vue
@@ -12,9 +12,9 @@
         <transition name="el-zoom-in-top">
           <div v-show="show" class="transition-box">
             <span>
-             提供网页服务 by 探索分享，开源项目见github，如需要key可以查看<a href="shop.iosshop.xyz">Shop探索分享OpenAI</a> 
+             提供网页服务 by 探索分享，开源项目见github，<a href="https://shop.iosshop.xyz?code=YT05JmI9MTI0" target="_blank" rel="noopener noreferrer"> 购买key：点击</a> 
             <div>
-              开源地址<a href="https://space.bilibili.com/326625155?spm_id_from=333.337.0.0">BliBili</a>---<a href="https://github.com/202252197/ChatGPT_JCM">GitHub</a>
+              <a href="https://space.bilibili.com/326625155?spm_id_from=333.337.0.0">BliBili</a>---<a href="https://github.com/202252197/ChatGPT_JCM">GitHub</a>
             </div>
             <div>
               <h3></h3>
